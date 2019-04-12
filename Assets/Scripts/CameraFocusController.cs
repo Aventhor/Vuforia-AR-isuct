d18daf8e5213a8bbb7dad85e582964a6c75628db
Aventhor <vaster399@gmail.com>
2019-04-12 22:40:20 +0300

    mendeleev edition

---
+++ Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var vufiria = VuforiaARController.Instance;
+		vufiria.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		vufiria.RegisterOnPauseCallback(OnPaused);
+	}
+	
+	private void OnVuforiaStarted() {
+		CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+	private void OnPaused(bool paused) {
+		if(!paused) {
+			CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
